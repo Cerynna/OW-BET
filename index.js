02f0992cdc37b7c06552ccbdde8deb505d7955bb
Jeremy Martin <jeremy@maans.eu>
2019-01-19 20:20:19 +0100

    firstCommit

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ app.post('/api/signIn', (req, res) => {
     
     // Handles any requests that don't match the ones above
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+        res.sendFile('/client/build/index.html');
     });
     
     const port = process.env.PORT || 5000;
