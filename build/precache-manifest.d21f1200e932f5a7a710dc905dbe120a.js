ad6df0e631fc407d07209c264264e87a8d6cb8fe
Jeremy Martin <jeremy@maans.eu>
2019-01-19 21:14:46 +0100

    WIP

---
+++ build/precache-manifest.d21f1200e932f5a7a710dc905dbe120a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff78c5e2b6c3846035dae28cdaa65583",
+    "url": "/static/media/big_noodle_titling.ff78c5e2.ttf"
+  },
+  {
+    "revision": "2c111f25c4c970897adb82e426a55069",
+    "url": "/static/media/back.2c111f25.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a76a816dcf83414d99a",
+    "url": "/static/js/main.1a76a816.chunk.js"
+  },
+  {
+    "revision": "0cde846d5828bf5ced4b",
+    "url": "/static/js/1.0cde846d.chunk.js"
+  },
+  {
+    "revision": "1a76a816dcf83414d99a",
+    "url": "/static/css/main.a0a335c5.chunk.css"
+  },
+  {
+    "revision": "a2c631d99c0cc5e66dd69d8f52778e9a",
+    "url": "/index.html"
+  }
+];
